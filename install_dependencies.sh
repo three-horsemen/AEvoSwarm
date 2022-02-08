993589cac62dc779e1fc6dbfa65ec29b6de1afa6
Reuben John <reubenvjohn@gmail.com>
2022-02-07 23:25:37 -0500

    Added install_dependencies.sh

---
+++ install_dependencies.sh
@@ -0,0 +1,9 @@
+# OpenCV dependencies - Details available at: http://docs.opencv.org/trunk/doc/tutorials/introduction/linux_install/linux_install.html
+sudo apt-get install -y build-essential
+sudo apt-get install -y cmake git libgtk2.0-dev pkg-config libavcodec-dev libavformat-dev libswscale-dev
+sudo apt-get install -y python-dev python-numpy libtbb2 libtbb-dev libjpeg-dev libpng-dev libtiff-dev libjasper-dev libdc1394-22-dev
+
+# Download v3.4.11 .zip file and extract.
+curl -sL https://github.com/Itseez/opencv/archive/3.4.11-beta.zip > opencv.zip
+unzip opencv.zip
+cd opencv-3.4.11 || exit

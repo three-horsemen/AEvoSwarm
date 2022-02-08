70f5bdff0519228d6db4afba40f688505c1fea31
Reuben John <reubenvjohn@gmail.com>
2022-02-07 23:37:26 -0500

    Added opencv installation script

--- install_dependencies.sh
+++ install_dependencies.sh
@@ -7,4 +7,24 @@ sudo apt-get install -y python-dev python-numpy libtbb2 libtbb-dev libjpeg-dev l
 # Download v3.4.11 .zip file and extract.
 curl -sL https://github.com/Itseez/opencv/archive/3.4.11.zip > opencv.zip
 unzip opencv.zip
-cd opencv-3.4.11
+
+cd opencv-3.4.11-beta
+
+  # Create a new 'build' folder.
+mkdir build_opencv
+cd build_opencv
+
+  # Set build instructions for Ubuntu distro.
+cmake -D CMAKE_BUILD_TYPE=RELEASE -D CMAKE_INSTALL_PREFIX=/usr/local -D BUILD_NEW_PYTHON_SUPPORT=ON -D INSTALL_C_EXAMPLES=OFF -D INSTALL_PYTHON_EXAMPLES=ON -D BUILD_EXAMPLES=ON -D ENABLE_PRECOMPILED_HEADERS=OFF ..
+
+  # Run 'make' with four threads.
+make -j4
+
+  # Install to OS.
+sudo make install
+
+  # Add configuration to OpenCV to tell it where the library files are located on the file system (/usr/local/lib)
+sudo sh -c 'echo "/usr/local/lib" > /etc/ld.so.conf.d/opencv.conf'
+
+sudo ldconfig
+echo "OpenCV installed."
